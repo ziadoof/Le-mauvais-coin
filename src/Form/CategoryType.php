9c9c3b005540610b4a08c76a990bd6cc9cac6838
ziadoof <ziadoof@gmail.com>
2018-07-14 20:10:46 +0200

    crud for all

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('class')
+            ->add('sp1')
+            ->add('sp2')
+            ->add('sp3')
+            ->add('sp4')
+            ->add('sp5')
+            ->add('sp6')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
